51750ef66fe1feff7d80f9c56aab5e13005e07c0
almutwakel <almutwakelhassan@gmail.com>
2021-10-02 17:20:08 -0400

    delete stuff

--- site/apiFunctions/UpdateTimeSlot.js
+++
@@ -1,9 +0,0 @@
-
-/*
-Add the specific time slot given the time slot ID to User
-
-Put on `${BACKEND_URL}/recipients/${SMC}/?id=${timeSlotID} assigns timeslot# to user with SMC using function “update”
-
-
-
-*/
