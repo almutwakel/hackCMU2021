1e0812d150cd9f7bee8011ad449ccf580721e34f
almutwakel <almutwakelhassan@gmail.com>
2021-10-01 22:14:40 -0400

    Initial data types

---
+++ my-project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bfc2301505410dc4bd003d950a6460d0'),
+    },
+  },
+});
---
+++ my-project/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '02912915-dd1a-4680-a056-8571273cfda0'
+};
