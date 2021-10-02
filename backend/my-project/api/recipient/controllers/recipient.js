f6cf7aadab832a5a8fd64726dc339e3d55d2c351
almutwakel <almutwakelhassan@gmail.com>
2021-10-02 13:28:59 -0400

    Add user to timeslot functionality

--- backend/my-project/api/recipient/controllers/recipient.js
+++ backend/my-project/api/recipient/controllers/recipient.js
@@ -1,4 +1,5 @@
-const { sanitizeEntity, convertRestQueryParams } = require('strapi-utils');
+const { parseMultipartData, sanitizeEntity, convertRestQueryParams } = require('strapi-utils');
+const { update } = require('../../timeslot/controllers/timeslot');
 
 module.exports = {
   /**
@@ -21,7 +22,25 @@ module.exports = {
         const SMC = ctx.SMC;
         const packages = ctx.packages;
         const timeslot = ctx.timeslot;
-        return {"SMC": SMC, "packages": packages, "timeslot": timeslot};
-}
+        const name = ctx.Name
+        return  ( { 
+            "User": name,
+            "SMCNumber": SMC, 
+            "packages": packages, 
+            "timeslot": timeslot
+        } );
+
+
+
+    },
+
+    async update(ctx) {
+        const { SMC } = ctx.params;
+    
+        let entity;
+        entity = await strapi.services.recipient.update({ SMC }, ctx.request.body);
+    
+        return sanitizeEntity(entity, { model: strapi.models.recipient });
+      },
 
 };
